e2dd4631aa491a378adc58f0ba8d424cf792a9d2
Luke Franzke <lukefranzke@gmail.com>
2022-10-06 14:35:15 +0200

    minor scaling fix

--- sketch.js
+++ sketch.js
@@ -3,13 +3,14 @@ let capture;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO);
-  capture.size(windowWidth, windowHeight);
+ // capture.size(windowWidth, windowHeight);
   //capture.hide();
 }
 
 function draw() {
-  background(255);
- image(capture, 0, 0, width, height);
+  background(0);
+  image(capture, 0, 0, windowWidth, width * capture.height / capture.width);
+  //image(capture, 0, 0, width, height);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
