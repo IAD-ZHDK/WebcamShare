e076c63cf98dedad517edeb6247a8e64a3d08508
Luke Franzke <lukefranzke@gmail.com>
2022-10-12 09:28:44 +0200

    updated sketch

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
 let capture;
 let devices = [];
+let mirrorFlag = false;
 let deviceID = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   setupCamera();
-
 }
 
 function draw() {
   background(0);
-  if (typeof(capture.loadedmetadata) != "undefined" && capture.loadedmetadata) {
-  image(capture, 0, 0, windowWidth, width * capture.height / capture.width);
+  if (typeof (capture.loadedmetadata) != "undefined" && capture.loadedmetadata) {
+    if (mirrorFlag) {
+      push();
+      translate(windowWidth,0)
+      scale(-1, 1)
+      image(capture, 0, 0, windowWidth, width * capture.height / capture.width);
+      pop();
+    } else {
+      image(capture, 0, 0, windowWidth, width * capture.height / capture.width);
+    }
   }
 }
 function windowResized() {
@@ -18,9 +26,9 @@ function windowResized() {
 }
 
 function setupCamera() {
-   devices = [];
+  devices = [];
   navigator.mediaDevices.enumerateDevices()
-  .then(gotDevices);
+    .then(gotDevices);
 }
 
 function gotDevices(deviceInfos) {
@@ -47,16 +55,22 @@ function gotDevices(deviceInfos) {
 }
 
 function mouseClicked() {
-  setupContextmenu() 
+  setupContextmenu()
+}
+
+function keyPressed() {
+  if (keyCode == ENTER) {
+    mirrorFlag = !mirrorFlag;
+  }
 }
 
 function setupContextmenu() {
   let text;
   let deviceList = "All cameras:";
   for (let i = 0; i !== devices.length; ++i) {
-    deviceList = deviceList+"\n id: "+ i +" label: "+devices[i].label;
+    deviceList = deviceList + "\n id: " + i + " label: " + devices[i].label;
   }
-  let setID = prompt(deviceList+"\n \n Enter the desired camera ID", "0");
+  let setID = prompt(deviceList + "\n \n Enter the desired camera ID", "0");
 
   if (setID == null || setID == "") {
     console.log("User cancelled the prompt.");
